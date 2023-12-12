f9856ac30322e5af5b4eeb7d283f5c767ace5e97
Christian Stussak <stussak@mfo.de>
2023-12-12 10:58:06 +0100

    Regenerate circular economy model code from InsightMaker model

--- src/ts/circular-economy-model.ts
+++ src/ts/circular-economy-model.ts
@@ -7,44 +7,84 @@ import {
 } from './model';
 
 const stockIds = [
-  'manufacturer',
-  'firstHand',
-  'secondHand',
-  'hibernating',
-  'broken',
-  'landfill',
-] as const;
-
-const parameterIds = [
-  'firstHandPreference',
-  'globalDemand',
-  'abandonRate',
-  'breakRate',
-  'repairRate',
-  'reuseRate',
-  'refurbishRate',
-  'recycleRate',
+  'capacityOfNewlyProducedPhones',
+  'capacityOfRecycledMaterials',
+  'capacityOfRefurbishedPhones',
+  'capacityOfRepairedPhones',
+  'phonesInUse',
+  'supplyOfBrokenPhones',
+  'supplyOfDisposedPhones',
+  'supplyOfHibernatingPhones',
+  'supplyOfNewlyProducedPhones',
+  'supplyOfRecycledMaterials',
+  'supplyOfRefurbishedPhones',
+  'supplyOfRepairedPhones',
 ] as const;
 
 const flowIds = [
-  'abandonFirstHand',
-  'abandonSecondHand',
-  'breakFirstHand',
-  'breakSecondHand',
+  'abandon',
+  'acquireNewlyProduced',
+  'acquireRefurbished',
+  'acquireRepaired',
+  'acquireUsed',
+  'capacityOfNewlyProducedPhonesAdjustment',
+  'capacityOfRecycledMaterialsAdjustment',
+  'capacityOfRefurbishedPhonesAdjustment',
   'disposeBroken',
   'disposeHibernating',
-  'naturalResources',
+  'goBroken',
+  'landfill',
+  'produceFromNaturalResources',
+  'produceFromRecycledMaterials',
   'recycle',
   'refurbish',
   'repair',
-  'reuse',
-  'selling',
+  'repairShopCapcityAdjustment',
 ] as const;
 
 const variableIds = [
-  'firstHandDemand',
-  'secondHandDemand',
-  'numberOfItems',
+  'demandForNaturalResources',
+  'demandForNewlyProducedPhones',
+  'demandForPhones',
+  'demandForRecycledMaterials',
+  'demandForRefurbishedPhones',
+  'demandForRefurbishment',
+  'demandForRepairedPhones',
+  'demandForReusedPhones',
+  'inflowIncentiveSumForPhonesInUse',
+  'phoneGoal',
+  'phonesInUseExcess',
+  'recycleDemand',
+  'repairDemand',
+  'resourceDemand',
+  'supplyOfBrokenPhonesForDisposal',
+  'supplyOfBrokenPhonesForRepair',
+  'supplyOfDisposedPhonesForLandfilling',
+  'supplyOfDisposedPhonesForRecycling',
+] as const;
+
+const parameterIds = [
+  'abandonRate',
+  'acquireRate',
+  'breakRate',
+  'capacityAdjustmentRate',
+  'disposeIncentive',
+  'disposeRate',
+  'durability',
+  'landfillIncentive',
+  'landfillRate',
+  'naturalResourcesIncentive',
+  'newPhoneProductionRate',
+  'newlyProducedPhoneIncentive',
+  'numberOfUsers',
+  'phonesPerUserGoal',
+  'recycleRate',
+  'recyclingIncentive',
+  'refurbishmentIncentive',
+  'refurbishmentRate',
+  'repairIncentive',
+  'repairRate',
+  'reuseIncentive',
 ] as const;
 
 type StockIds = typeof stockIds;
@@ -76,23 +116,42 @@ class CircularEconomyModel extends Model<
   ParameterIds
 > {
   public static readonly initialStocks: Readonly<Stocks> = {
-    manufacturer: 0,
-    firstHand: 0,
-    secondHand: 0,
-    hibernating: 0,
-    broken: 0,
-    landfill: 0,
+    capacityOfNewlyProducedPhones: 0,
+    capacityOfRecycledMaterials: 0,
+    capacityOfRefurbishedPhones: 0,
+    capacityOfRepairedPhones: 0,
+    phonesInUse: 0,
+    supplyOfBrokenPhones: 0,
+    supplyOfDisposedPhones: 0,
+    supplyOfHibernatingPhones: 0,
+    supplyOfNewlyProducedPhones: 0,
+    supplyOfRecycledMaterials: 0,
+    supplyOfRefurbishedPhones: 0,
+    supplyOfRepairedPhones: 0,
   };
 
   public static readonly defaultParameters: Readonly<Parameters> = {
-    firstHandPreference: 0.52,
-    globalDemand: 1000000,
-    abandonRate: 0.0,
-    breakRate: 0.25,
-    repairRate: 1.0,
-    reuseRate: 1.0,
-    refurbishRate: 1.0,
-    recycleRate: 0.0,
+    abandonRate: 0.1,
+    acquireRate: 1,
+    breakRate: 0.1,
+    capacityAdjustmentRate: 0.1,
+    disposeIncentive: 0.5,
+    disposeRate: 0.5,
+    durability: 1,
+    landfillIncentive: 0.5,
+    landfillRate: 1,
+    naturalResourcesIncentive: 0.5,
+    newPhoneProductionRate: 0.5,
+    newlyProducedPhoneIncentive: 0.82,
+    numberOfUsers: 1000000,
+    phonesPerUserGoal: 1,
+    recycleRate: 1,
+    recyclingIncentive: 0.89,
+    refurbishmentIncentive: 0.5,
+    refurbishmentRate: 1,
+    repairIncentive: 0.5,
+    repairRate: 1,
+    reuseIncentive: 0.86,
   };
 
   constructor() {
@@ -101,49 +160,175 @@ class CircularEconomyModel extends Model<
 
   // eslint-disable-next-line class-methods-use-this
   public evaluate(stocks: Stocks, parameters: Parameters, t: number): Record {
-    const { firstHand, secondHand, hibernating, broken } = stocks;
     const {
-      firstHandPreference,
-      globalDemand,
+      capacityOfNewlyProducedPhones,
+      capacityOfRecycledMaterials,
+      capacityOfRefurbishedPhones,
+      capacityOfRepairedPhones,
+      phonesInUse,
+      supplyOfBrokenPhones,
+      supplyOfDisposedPhones,
+      supplyOfHibernatingPhones,
+      supplyOfNewlyProducedPhones,
+      supplyOfRecycledMaterials,
+      supplyOfRefurbishedPhones,
+      supplyOfRepairedPhones,
+    } = stocks;
+    const {
       abandonRate,
+      acquireRate,
       breakRate,
-      repairRate,
-      reuseRate,
-      refurbishRate,
+      capacityAdjustmentRate,
+      disposeIncentive,
+      disposeRate,
+      durability,
+      landfillIncentive,
+      landfillRate,
+      naturalResourcesIncentive,
+      newPhoneProductionRate,
+      newlyProducedPhoneIncentive,
+      numberOfUsers,
+      phonesPerUserGoal,
       recycleRate,
+      recyclingIncentive,
+      refurbishmentIncentive,
+      refurbishmentRate,
+      repairIncentive,
+      repairRate,
+      reuseIncentive,
     } = parameters;
 
-    const abandonFirstHand = abandonRate * firstHand;
-    const abandonSecondHand = abandonRate * secondHand;
-    const breakFirstHand = breakRate * firstHand;
-    const breakSecondHand = breakRate * secondHand;
-    const disposeBroken = 0.4 * broken;
-    const disposeHibernating = 0.4 * hibernating;
-    const naturalResources = 0.0;
-    const recycle = recycleRate * broken;
-    const refurbish = refurbishRate * hibernating;
-    const firstHandDemand = globalDemand * firstHandPreference;
-    const secondHandDemand = globalDemand - firstHandDemand;
-    const numberOfItems = firstHand + secondHand;
-    const repair = Math.min(secondHandDemand - secondHand, broken) * repairRate;
-    const reuse =
-      Math.min(secondHandDemand - secondHand, hibernating) * reuseRate;
-    const selling = firstHandDemand - firstHand;
+    const phoneGoal = numberOfUsers * phonesPerUserGoal;
+    const phonesInUseExcess = Math.max(phonesInUse - phoneGoal, 0);
+    const abandon = abandonRate * phonesInUseExcess;
+    const inflowIncentiveSumForPhonesInUse =
+      reuseIncentive +
+      repairIncentive +
+      refurbishmentIncentive +
+      newlyProducedPhoneIncentive;
+    const demandForPhones = Math.max(0, phoneGoal - phonesInUse);
+    const demandForNewlyProducedPhones =
+      (newlyProducedPhoneIncentive / inflowIncentiveSumForPhonesInUse) *
+      demandForPhones;
+    const acquireNewlyProduced =
+      acquireRate *
+      Math.min(supplyOfNewlyProducedPhones, demandForNewlyProducedPhones);
+    const demandForRefurbishedPhones =
+      (refurbishmentIncentive / inflowIncentiveSumForPhonesInUse) *
+      demandForPhones;
+    const acquireRefurbished =
+      acquireRate *
+      Math.min(demandForRefurbishedPhones, supplyOfRefurbishedPhones);
+    const demandForRepairedPhones =
+      (repairIncentive / inflowIncentiveSumForPhonesInUse) * demandForPhones;
+    const acquireRepaired =
+      acquireRate * Math.min(demandForRepairedPhones, supplyOfRepairedPhones);
+    const demandForReusedPhones =
+      (reuseIncentive / inflowIncentiveSumForPhonesInUse) * demandForPhones;
+    const acquireUsed =
+      acquireRate * Math.min(supplyOfHibernatingPhones, demandForReusedPhones);
+    const capacityOfNewlyProducedPhonesAdjustment =
+      capacityAdjustmentRate *
+      (demandForNewlyProducedPhones - capacityOfNewlyProducedPhones);
+    const resourceDemand = Math.max(
+      capacityOfNewlyProducedPhones - supplyOfNewlyProducedPhones,
+      0,
+    );
+    const demandForRecycledMaterials =
+      (recyclingIncentive / (naturalResourcesIncentive + recyclingIncentive)) *
+      resourceDemand;
+    const capacityOfRecycledMaterialsAdjustment =
+      capacityAdjustmentRate *
+      (demandForRecycledMaterials - capacityOfRecycledMaterials);
+    const capacityOfRefurbishedPhonesAdjustment =
+      capacityAdjustmentRate *
+      (demandForRefurbishedPhones - capacityOfRefurbishedPhones);
+    const supplyOfBrokenPhonesForDisposal =
+      (disposeIncentive / (repairIncentive + disposeIncentive)) *
+      supplyOfBrokenPhones;
+    const disposeBroken = disposeRate * supplyOfBrokenPhonesForDisposal;
+    const disposeHibernating = disposeRate * supplyOfHibernatingPhones;
+    const goBroken = (breakRate / durability) * phonesInUse;
+    const supplyOfDisposedPhonesForLandfilling =
+      (landfillIncentive / (recyclingIncentive + landfillIncentive)) *
+      supplyOfDisposedPhones;
+    const landfill = landfillRate * supplyOfDisposedPhonesForLandfilling;
+    const demandForNaturalResources =
+      (naturalResourcesIncentive /
+        (naturalResourcesIncentive + recyclingIncentive)) *
+      resourceDemand;
+    const produceFromNaturalResources =
+      newPhoneProductionRate * demandForNaturalResources;
+    const produceFromRecycledMaterials =
+      newPhoneProductionRate *
+      Math.min(demandForRecycledMaterials, supplyOfRecycledMaterials);
+    const supplyOfDisposedPhonesForRecycling =
+      (recyclingIncentive / (recyclingIncentive + landfillIncentive)) *
+      supplyOfDisposedPhones;
+    const recycleDemand = Math.max(
+      capacityOfRecycledMaterials - supplyOfRecycledMaterials,
+      0,
+    );
+    const recycle =
+      recycleRate * Math.min(supplyOfDisposedPhonesForRecycling, recycleDemand);
+    const demandForRefurbishment = Math.max(
+      capacityOfRefurbishedPhones - supplyOfRefurbishedPhones,
+      0,
+    );
+    const refurbish =
+      refurbishmentRate *
+      Math.min(supplyOfHibernatingPhones, demandForRefurbishment);
+    const repairDemand = Math.max(
+      capacityOfRepairedPhones - supplyOfRepairedPhones,
+      0,
+    );
+    const repair = repairRate * Math.min(supplyOfBrokenPhones, repairDemand);
+    const repairShopCapcityAdjustment =
+      capacityAdjustmentRate *
+      (demandForRepairedPhones - capacityOfRepairedPhones);
+    const supplyOfBrokenPhonesForRepair =
+      (repairIncentive / (repairIncentive + disposeIncentive)) *
+      supplyOfBrokenPhones;
 
-    const variables = { firstHandDemand, secondHandDemand, numberOfItems };
+    const variables = {
+      demandForNaturalResources,
+      demandForNewlyProducedPhones,
+      demandForPhones,
+      demandForRecycledMaterials,
+      demandForRefurbishedPhones,
+      demandForRefurbishment,
+      demandForRepairedPhones,
+      demandForReusedPhones,
+      inflowIncentiveSumForPhonesInUse,
+      phoneGoal,
+      phonesInUseExcess,
+      recycleDemand,
+      repairDemand,
+      resourceDemand,
+      supplyOfBrokenPhonesForDisposal,
+      supplyOfBrokenPhonesForRepair,
+      supplyOfDisposedPhonesForLandfilling,
+      supplyOfDisposedPhonesForRecycling,
+    };
     const flows = {
-      abandonFirstHand,
-      abandonSecondHand,
-      breakFirstHand,
-      breakSecondHand,
+      abandon,
+      acquireNewlyProduced,
+      acquireRefurbished,
+      acquireRepaired,
+      acquireUsed,
+      capacityOfNewlyProducedPhonesAdjustment,
+      capacityOfRecycledMaterialsAdjustment,
+      capacityOfRefurbishedPhonesAdjustment,
       disposeBroken,
       disposeHibernating,
-      naturalResources,
+      goBroken,
+      landfill,
+      produceFromNaturalResources,
+      produceFromRecycledMaterials,
       recycle,
       refurbish,
       repair,
-      reuse,
-      selling,
+      repairShopCapcityAdjustment,
     };
 
     return { t, stocks, parameters, variables, flows };
@@ -152,32 +337,54 @@ class CircularEconomyModel extends Model<
   // eslint-disable-next-line class-methods-use-this
   public accumulateFlowsPerStock(flows: Flows): Stocks {
     const {
-      abandonFirstHand,
-      abandonSecondHand,
-      breakFirstHand,
-      breakSecondHand,
+      abandon,
+      acquireNewlyProduced,
+      acquireRefurbished,
+      acquireRepaired,
+      acquireUsed,
+      capacityOfNewlyProducedPhonesAdjustment,
+      capacityOfRecycledMaterialsAdjustment,
+      capacityOfRefurbishedPhonesAdjustment,
       disposeBroken,
       disposeHibernating,
-      naturalResources,
+      goBroken,
+      landfill,
+      produceFromNaturalResources,
+      produceFromRecycledMaterials,
       recycle,
       refurbish,
       repair,
-      reuse,
-      selling,
+      repairShopCapcityAdjustment,
     } = flows;
+
     const flowPerStock: Stocks = {
-      manufacturer: naturalResources + recycle - selling,
-      firstHand: selling + refurbish - (abandonFirstHand + breakFirstHand),
-      secondHand: repair + reuse - (abandonSecondHand + breakSecondHand),
-      hibernating:
-        abandonFirstHand + abandonSecondHand - (disposeHibernating + refurbish),
-      broken: breakFirstHand + breakSecondHand - (disposeBroken + repair),
-      landfill: disposeHibernating + disposeBroken,
+      capacityOfNewlyProducedPhones: capacityOfNewlyProducedPhonesAdjustment,
+      capacityOfRecycledMaterials: capacityOfRecycledMaterialsAdjustment,
+      capacityOfRefurbishedPhones: capacityOfRefurbishedPhonesAdjustment,
+      capacityOfRepairedPhones: repairShopCapcityAdjustment,
+      phonesInUse:
+        acquireNewlyProduced +
+        acquireRefurbished +
+        acquireRepaired +
+        acquireUsed -
+        (abandon + goBroken),
+      supplyOfBrokenPhones: goBroken - (disposeBroken + repair),
+      supplyOfDisposedPhones:
+        disposeBroken + disposeHibernating - (landfill + recycle),
+      supplyOfHibernatingPhones:
+        abandon - (acquireUsed + disposeHibernating + refurbish),
+      supplyOfNewlyProducedPhones:
+        produceFromNaturalResources +
+        produceFromRecycledMaterials -
+        acquireNewlyProduced,
+      supplyOfRecycledMaterials: recycle - produceFromRecycledMaterials,
+      supplyOfRefurbishedPhones: refurbish - acquireRefurbished,
+      supplyOfRepairedPhones: repair - acquireRepaired,
     };
+
     return flowPerStock;
   }
 }
-
 export type {
   StockIds,
   StockId,
